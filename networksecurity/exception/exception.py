3fcf563f0d08b4a0299780027cc04dabd271fad1
lokesh-saragadam <lokeshs2k6@gmail.com>
2025-04-23 15:26:23 +0530

    logging and exception

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,27 @@
+from networksecurity.logging import logger
+import sys
+
+class NetworkSecurityException(Exception):
+     def __init__(self,errorr_message,error_details:sys):
+          self.error_message = errorr_message
+          _,_,exc_tb= error_details.exc_info()
+
+          self.lineno = exc_tb.tb_lineno
+          self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+     def __str__(self):
+          return "Error Occured in Python script name [{0}] line number [{1}] error message [{2}]"(
+          self.file_name,self.lineno,str(self.error_message))
+
+if __name__ == "__main__":
+     try :
+          logger.logging.info("The exception has started")
+          a=1/0
+     except Exception as e:
+          raise NetworkSecurityException(e,sys)
+
+
+
+
+
+
