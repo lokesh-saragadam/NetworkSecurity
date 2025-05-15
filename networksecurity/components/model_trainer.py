a72f71c77cbc0be1fb811489cc5920e58230ef77
lokesh-saragadam <lokeshs2k6@gmail.com>
2025-05-15 19:10:07 +0530

    app.py added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -21,6 +21,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
+import dagshub
+dagshub.init(repo_owner='lokesh-saragadam', repo_name='NetworkSecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
